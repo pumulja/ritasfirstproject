6b4d22309da3ede9d396d7955e18709aaa9598fe
rita <pumulja@mail.ru>
2018-04-01 23:28:08 +0300

    added prev

--- Js2.js
+++ Js2.js
@@ -12,3 +12,18 @@ const my_button = document.getElementById('join_newsletter');
 my_button.onclick = function(){
   event.preventDefault(); // отменить действие браузера по умолчанию
   console.log(my_button)
+
+  // при нажатии на кнопку действие происходило
+const my_button = document.getElementById('join_newsletter');
+
+my_button.onclick = function(){
+  event.preventDefault(); // отменить действие браузера по умолчанию
+
+  const name = document.getElementById('name');
+
+  if (name.value === ""){
+    name.style.border = '1px solid red';
+  } else {
+    name.style.border = '1px solid black';
+  }
+}
